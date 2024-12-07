c60d20a655887f5fec2da73dfd14bcaa5483e689
EDUCACION <educacion@MacBook-Pro-de-EDUCACION.local>
2024-12-06 18:37:01 -0600

    Fin

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBarComponent from './Navbar/NavBarComponent';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
 function App() {
-  return <>test</>;
+  return (
+    <Router>
+      <div className="App">
+        <NavBarComponent />
+        <Routes>
+         
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
