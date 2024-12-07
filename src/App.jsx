9059090b024ac8e70ceb7751dc137be32ae5ee7d
Erick <erick.vega@hidalgo.gob.mx>
2024-12-06 18:45:38 -0600

    Se configura Router del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./components/home/Home";
+
 function App() {
-  return <></>;
+  return <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  </>;
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+import Navbar from './components/navbar/Navbar.jsx'
+import Footer from './components/footer/Footer.jsx'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Navbar />
     <App />
+    <Footer />
   </StrictMode>,
 )
